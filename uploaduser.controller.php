058e2a28b0219e505600921c65ea8f7d8e4cdb1a
valery <valery.fremaux@gmail.com>
2018-04-10 23:27:05 +0200

    split community / pro version

--- uploaduser.controller.php
+++ uploaduser.controller.php
@@ -26,7 +26,7 @@ require_once($CFG->dirroot.'/blocks/user_delegation/classes/userdelegation.class
 require_once($CFG->dirroot.'/user/profile/lib.php');
 
 // Fix the weird POST bounce form field loss when ajax changing a form.
-$data->grouptoassign = $_REQUEST['grouptoassign'];
+$data->grouptoassign = clean_param(@$_REQUEST['grouptoassign'], PARAM_TEXT);
 
 $fs = get_file_storage();
 $usercontext = context_user::instance($USER->id);
@@ -267,7 +267,7 @@ if (!$fs->is_area_empty($usercontext->id, 'user', 'draft', $data->userfile)) {
 
                 } else if ($name == 'username') {
                     // Username (escape and force lowercase).
-                    $user->username = textlib::strtolower($value);
+                    $user->username = core_text::strtolower($value);
 
                 } else {
                     // Normal entry (escape only).
@@ -393,84 +393,10 @@ if (!$fs->is_area_empty($usercontext->id, 'user', 'draft', $data->userfile)) {
             // Add the uploaded/updated user on behalf of the uploader.
             userdelegation::attach_user($USER->id, $user->id);
 
-            $coursegroup = null;
-
             // Assign it to the selected course if any.
             if (!empty($data->coursetoassign)) {
-                $studentrole = $DB->get_record('role', array('shortname' => 'student'));
-                $course = $DB->get_record('course', array('id' => $data->coursetoassign));
-                $coursecontext = context_course::instance($course->id);
-
-                // Compute enrolment end time if required by block config.
-                $end = 0;
-                if (!empty($theblock->config->enrolduration)) {
-                    $end = time() + DAYSECS * $theblock->config->enrolduration;
-                }
-
-                if ($course) {
-                    // TODO : Rewrite assignation.
-                    $params = array('enrol' => 'manual', 'courseid' => $course->id, 'status' => ENROL_INSTANCE_ENABLED);
-                    if ($enrols = $DB->get_records('enrol', $params, 'sortorder ASC')) {
-                        $enrol = reset($enrols);
-                        $enrolplugin = enrol_get_plugin('manual');
-                        $enrolplugin->enrol_user($enrol, $user->id, $studentrole->id, time(), $end, ENROL_USER_ACTIVE);
-                        $message = get_string('userenrolled', 'block_user_delegation', $course->shortname);
-                        $log .= useradmin_uploaduser_notify_success($linenum, $message, $user->id, $user->username);
-                    }
-                }
-
-                if (is_null($coursegroup) && !empty($data->newgroupname)) {
-                    $newgroup = new Stdclass();
-                    $newgroup->courseid = $course->id;
-                    $newgroup->name = $data->newgroupname;
-                    $newgroup->description = '';
-                    $newgroup->descriptionformat = 0;
-                    $newgroup->timecreated = time();
-                    $newgroup->timemodified = time();
-                    $params = array('courseid' => $newgroup->courseid, 'name' => $newgroup->name);
-                    if (!$coursegroup = $DB->get_record('groups', $params)) {
-                        $message = get_string('groupcreated', 'block_user_delegation', $newgroup->name);
-                        $log .= useradmin_uploaduser_notify_success($linenum, $message, $user->id, $user->username);
-                        $newgroup->id = $DB->insert_record('groups', $newgroup);
-
-                        // Invalidate the grouping cache for the course.
-                        cache_helper::invalidate_by_definition('core', 'groupdata', array(), array($course->id));
-
-                        // Trigger group event.
-                        $params = array(
-                            'context' => $coursecontext,
-                            'objectid' => $newgroup->id
-                        );
-                        $event = \core\event\group_created::create($params);
-                        $event->trigger();
-                        $coursegroup = $newgroup;
-                    }
-
-                    // Self enrol ourself in the created group to take control.
-                    $groupmember = new StdClass();
-                    $groupmember->groupid = $coursegroup->id;
-                    $groupmember->userid = $USER->id;
-                    $groupmember->timeadded = time();
-                    if (!$DB->record_exists('groups_members', array('groupid' => $coursegroup->id, 'userid' => $USER->id))) {
-                        $DB->insert_record('groups_members', $groupmember);
-                        $message = get_string('groupadded', 'block_user_delegation', $coursegroup->name);
-                        $log .= useradmin_uploaduser_notify_success($linenum, $message, $USER->id, $USER->username);
-                    }
-                } else if (!empty($data->grouptoassign)) {
-                    $coursegroup = $DB->get_record('groups', array('id' => $data->grouptoassign));
-                }
-    
-                if (!is_null($coursegroup)) {
-                    $groupmember = new StdClass();
-                    $groupmember->groupid = $coursegroup->id;
-                    $groupmember->userid = $user->id;
-                    $groupmember->timeadded = time();
-                    if (!$DB->record_exists('groups_members', array('groupid' => $coursegroup->id, 'userid' => $user->id))) {
-                        $DB->insert_record('groups_members', $groupmember);
-                        $message = get_string('groupadded', 'block_user_delegation', $coursegroup->name);
-                        $log .= useradmin_uploaduser_notify_success($linenum, $message, $user->id, $user->username);
-                    }
-                }
+                include_once($CFG->dirroot.'/blocks/user_delegation/pro/lib.php');
+                block_user_delegation_enrol($data, $user, $theblock);
             }
 
             $user = userdelegation::pre_process_custom_profile_data($user);
