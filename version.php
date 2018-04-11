a69985a3bcf3f8e0149d1da48d5e7012b0aa1959
valery <valery.fremaux@gmail.com>
2018-04-11 12:09:24 +0200

    fix pro release marker

--- version.php
+++ version.php
@@ -34,4 +34,4 @@ $plugin->release = '3.2.0 (Build 2017021300)';
 
 // Non moodle attributes.
 $plugin->codeincrement = '3.2.0002';
-$plugin->privacy = 'dual_release';
+$plugin->privacy = 'dualrelease';
